1b4618b7ee55de3f41e50d8fd73d8c25cd25db09
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-09-06 19:50:32 +0900

    feat: 기술스택 부분 작성

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,9 +1,34 @@
 import React, { forwardRef } from "react";
 
 function Skills(props: any, ref: React.Ref<HTMLDivElement>) {
+  const skillList = [
+    "HTML",
+    "React",
+    "Next.js",
+    "JavaScript",
+    "TypeScript",
+    "CSS",
+    "Tailwind-CSS",
+    "React-Query",
+    "Recoil",
+    "Zustand",
+    "Date-FNS",
+    "StroyBook",
+  ];
+
   return (
     <div id="skills" ref={ref} className="h-screen scroll-mt-60 border-b p-8">
       <h2 className="text-2xl font-semibold">기술스택</h2>
+      <div className="mt-40 flex flex-wrap gap-12">
+        {skillList.map((skill) => (
+          <div
+            key={skill}
+            className="rounded-15 border-[1px] border-solid border-text-secondary px-10"
+          >
+            {skill}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
