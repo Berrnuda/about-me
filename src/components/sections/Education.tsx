113c18ab24435c90140141dc324cc67b142ec74b
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-09-06 20:40:52 +0900

    feat: 교육 부분 작성

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -4,6 +4,11 @@ function Education(props: any, ref: React.Ref<HTMLDivElement>) {
   return (
     <div id="education" ref={ref} className="h-screen scroll-mt-60 p-8">
       <h2 className="text-2xl font-semibold">교육</h2>
+      <div className="mt-40 flex flex-col gap-8">
+        <p className="text-2xl">코드잇</p>
+        <p>코드잇 스프린트 프론트엔드 6기</p>
+        <p>24.03.07 ~ 24.09.02</p>
+      </div>
     </div>
   );
 }
