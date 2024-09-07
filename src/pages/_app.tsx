acdb7ad4d5cc6cb86d797ca468af3c6f7d3b799c
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-09-07 18:32:28 +0900

    docs: 메타데이터 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import ModalWrapper from "@/components/modal/ModalWrapper";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>About-Bermuda</title>
+        <meta name="description" content="나에 대해서 소개합니다." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="About-Bermuda" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://about-bermuda.vercel.app/" />
+        <meta property="og:image" content="/public/images/Profile.jpg" />
+      </Head>
       <ModalWrapper />
       <Component {...pageProps} />
     </>
