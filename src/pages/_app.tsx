a57355e2b34fed6c29bde0fde78e902c00e03af3
Berrnuda <162148781+Berrnuda@users.noreply.github.com>
2024-09-09 02:51:29 +0900

    feat: 구글 애널리틱스 기능 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import ModalWrapper from "@/components/modal/ModalWrapper";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,6 +22,15 @@ export default function App({ Component, pageProps }: AppProps) {
 
         <meta property="og:description" content="나에 대해서 소개합니다." />
       </Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VBM4JYW41G" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VBM4JYW41G');
+        `}
+      </Script>
       <ModalWrapper />
       <Component {...pageProps} />
     </>
