714dec49a43d7bdabae88eae28b8b48900054b77
김제완 <gimjewan@CarpeDiem-MacBookAir.local>
2024-09-16 03:53:30 +0900

    refactor: 링크프리뷰 컴포넌트 타입 추가

--- src/components/linkpreview/index.tsx
+++ src/components/linkpreview/index.tsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 import React from "react";
 const Profile = "/images/Profile.jpg";
 
+type link = {
+  title: string;
+  image: string;
+};
+
 export default function LinkPreview({ url }: { url: string }) {
-  const { data } = useQuery({
+  const { data } = useQuery<link>({
     queryKey: ["linkPreview", url],
     queryFn: () => fetchPreview(url),
     staleTime: Infinity,
@@ -28,11 +33,11 @@ export default function LinkPreview({ url }: { url: string }) {
       className="flex h-60 w-full max-w-720 justify-between gap-6 rounded-4 border border-text-secondary md:max-w-1000 xl:max-w-280"
     >
       <div className="flex h-50 flex-col overflow-y-hidden p-4">
-        <h3 className="font-bold">{data.title}</h3>
+        <h3 className="font-bold">{data?.title}</h3>
         {/* <p className="text-sm">{preview.description}</p> */}
         {/* <p className="text-sm text-text-secondary">{preview.url}</p> */}
       </div>
-      <img src={data.image ?? Profile} alt={data.title} className="h-58 w-100 object-cover" />
+      <img src={data?.image ?? Profile} alt={data?.title} className="h-58 w-100 object-cover" />
     </Link>
   );
 }
