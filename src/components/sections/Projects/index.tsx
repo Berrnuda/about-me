941c71af4605c7223d92ac057fd04b287a908c74
김제완 <gimjewan@CarpeDiem-MacBookAir.local>
2024-10-10 02:08:17 +0900

    refactor: 프로젝트 순서 변경

--- src/components/sections/Projects/index.tsx
+++ src/components/sections/Projects/index.tsx
@@ -67,22 +67,6 @@ function Projects(props: any, ref: React.Ref<HTMLDivElement>) {
       <h2 className="text-xl font-semibold md:text-2xl xl:text-3xl">프로젝트</h2>
       <div className="mt-10 grid gap-6 md:mt-20 md:grid-cols-2 xl:mt-40 xl:grid-cols-3">
         <PortfolioCard
-          icon={OpenMindIcon}
-          title="오픈마인드"
-          description="질문과 답변을 통해 마음을 열고 대화 나누는 소통 플랫폼"
-          skills={["React", "JavaScript", "Axios", "Css"]}
-          onClick={ModalOpenMind}
-          day="24.04.30 ~ 24.05.17"
-        />
-        <PortfolioCard
-          icon={ArbabaIcon}
-          title="아르바바와 40인의 사장들"
-          description="사장님도 알바생도 이용하는, 기존보다 높은 시급으로 알바를 빠르게 구할 수 있는 서비스"
-          skills={["Next", "TypeScript", "Axios", "Tailwind", "Recoil"]}
-          onClick={ModalArbaba}
-          day="24.06.21 ~ 24.07.07"
-        />
-        <PortfolioCard
           icon={TemagotchiIcon}
           title="티마고치"
           description="팀을 생성하고 팀원을 구성하여 공통의 할 일을 만들어서 업무의 효율을 높이는 프로젝트"
@@ -98,6 +82,22 @@ function Projects(props: any, ref: React.Ref<HTMLDivElement>) {
           ]}
           onClick={ModalTemagotchiMind}
           day="24.07.25 ~ 24.08.27"
+        />
+        <PortfolioCard
+          icon={ArbabaIcon}
+          title="아르바바와 40인의 사장들"
+          description="사장님도 알바생도 이용하는, 기존보다 높은 시급으로 알바를 빠르게 구할 수 있는 서비스"
+          skills={["Next", "TypeScript", "Axios", "Tailwind", "Recoil"]}
+          onClick={ModalArbaba}
+          day="24.06.21 ~ 24.07.07"
+        />
+        <PortfolioCard
+          icon={OpenMindIcon}
+          title="오픈마인드"
+          description="질문과 답변을 통해 마음을 열고 대화 나누는 소통 플랫폼"
+          skills={["React", "JavaScript", "Axios", "Css"]}
+          onClick={ModalOpenMind}
+          day="24.04.30 ~ 24.05.17"
         />
       </div>
     </div>
